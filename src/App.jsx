5af15c6f97fc56a03f16c8bf023b07675d7197c4
danish7866-git <danish7866-git>
2026-03-29 07:14:07 +0530

    left side part completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div className='h-screen w-screen bg-[url("https://i.pinimg.com/1200x/af/77/1c/af771cc65e15111095e50784786b21e5.jpg")] bg-cover'>
-
-     {/* Navbar */}
+      {/* Navbar */}
       <div className="py-5 px-10 flex items-center justify-between">
         {/* LOGO */}
         <div className="flex flex-col items-center">
@@ -27,8 +26,29 @@ const App = () => {
           </div>
         </div>
       </div>
-    </div>
-  )
-}
 
-export default App
+      {/* Main-content */}
+      <div className="w-full flex items-center gap-4">
+        {/* Left-content */}
+        <div className="p-5 pl-10 mt-5">
+          <h1 className="text-4xl text-white  tracking-tighter whitespace-nowrap font-bold uppercase font-[font1]">
+            limited collection
+          </h1>
+          <h1 className="text-8xl bg-gradient-to-br from-[#3d2b1a] via-[#C9A96E] to-[#DFDBD0] bg-clip-text text-transparent mt-5  whitespace-nowrap font-bold uppercase font-[font2]">
+            classic <br /> watch
+          </h1>
+          <h1 className="font-thin text-white uppercase tracking-widest ml-2 mt-3 text-xl">
+            starting in the swiss mountains
+          </h1>
+          <div className="w-50 rounded-full h-1 bg-gradient-to-br from-[#3d2b1a] via-[#C9A96E] to-[#DFDBD0] mt-8" />
+          <p className="text-lg w-120 font-medium mt-2 pr-10 text-white font-[font1] uppercase">
+            A symphony of precision and elegance , this watch is a marvel of
+            modern horology. Its heart beats with the Caliber 3235.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
